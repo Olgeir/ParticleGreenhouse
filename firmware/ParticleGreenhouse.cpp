2bc28705f5e6bc39c6bb77deb5a782272b5b8cdd
Olgeir Gunnsteinsson <OlgeirMac@Olgeirs-MacBook-Pro.local>
2015-11-15 00:43:52 +0100

    Took out const

--- firmware/ParticleGreenhouse.cpp
+++ firmware/ParticleGreenhouse.cpp
@@ -1,16 +1,17 @@
 #include "ParticleGreenhouse.h"
+#include "math.h"
 // First, define the pins we are going to use.
-const uint8_t STATUS_LED = D5;
-const uint8_t BUTTON = D3;
-const uint8_t BLUE_LED = D7; 	// This one is the little blue LED on your board. On the Photon it is next to D7, and on the Core it is next to the USB jack.
-const uint8_t D_MOIST = D2;  	// Digital input from moisture sensor.
-const uint8_t A_MOIST = A2;  	// Analog input from moisture sensor.
-const uint8_t POTENTION = A1; 	// Analog potentionmeter.
-const uint8_t EXT_TEMP = A0; 	// External temperature measurements.
-const uint8_t ON_TEMP = D4;	// Onboard temperature measurements.
-const uint8_t MOTOR = A6;		// Control signal for external motor.
-const uint8_t RELAY = A5;		// Control signal for relay.
-const uint8_t SERVO = A4;		// Control signal for servo.
+uint8_t STATUS_LED = D5;
+uint8_t BUTTON = D3;
+uint8_t BLUE_LED = D7; 	// This one is the little blue LED on your board. On the Photon it is next to D7, and on the Core it is next to the USB jack.
+uint8_t D_MOIST = D2;  	// Digital input from moisture sensor.
+uint8_t A_MOIST = A2;  	// Analog input from moisture sensor.
+uint8_t POTENTION = A1; 	// Analog potentionmeter.
+uint8_t EXT_TEMP = A0; 	// External temperature measurements.
+uint8_t ON_TEMP = D4;	// Onboard temperature measurements.
+uint8_t MOTOR = A6;		// Control signal for external motor.
+uint8_t RELAY = A5;		// Control signal for relay.
+uint8_t SERVO = A4;		// Control signal for servo.
 
 ParticleGreenhouse::ParticleGreenhouse(){
 	pinMode(STATUS_LED, OUTPUT);
