5882d704500cd5f91c83600769412680d65f7c24
Olgeir Gunnsteinsson <OlgeirMac@Olgeirs-MacBook-Pro.local>
2015-11-15 22:42:00 +0100

    set up an example code.

--- firmware/examples/GreenhouseControl.ino
+++ firmware/examples/GreenhouseControl.ino
@@ -1,12 +1,20 @@
-#include <ParticleGreenhouse.h>
+// This #include statement was automatically added by the Particle IDE.
+#include "ParticleGreenhouse/ParticleGreenhouse.h"
+
 
 
 ParticleGreenhouse control;
+//Servo myServo;
 
+//int SERVOPIN = A4;		// Control signal for servo.
 void setup() {
 
 }
 
 void loop() {
- control.blinkLed(2000);
-}
+    if(1000 > control.getPotMeter()){
+        if(control.buttonCheck()){
+            control.blinkLed(2000,1);
+        }
+    }
+ }
